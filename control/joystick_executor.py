b1c67dd631d9a0992b477527f60f947846dbbbac
Chris Thierauf <chris@cthierauf.com>
2019-07-15 15:03:46 -0400

    Get basic server going

--- control/joystick_executor.py
+++ control/joystick_executor.py
@@ -20,11 +20,54 @@
 """
 
 import rospy
+from autobahn.twisted.websocket import WebSocketServerFactory, WebSocketServerProtocol
+from twisted.python import reactor
 from auv.msg import trajectory
 from auv.msg import mode
 
-pub = rospy.Publisher('joystick_execution', trajectory, queue_size=3)
+trajpub = rospy.Publisher('joystick_execution', trajectory, queue_size=3)
 modepub = rospy.Publisher('mode_request', mode, queue_size=3)
+
+# Yoinked from surface_station/server/server.py 
+class BroadcastServerProtocol(WebSocketServerProtocol):
+
+    def onOpen(self):
+        self.factory.register(self)
+                                                                                                                    
+    def onMessage(self, payload, isBinary):
+        payload = b"Server response: " + payload
+        self.factory.broadcast(payload, isBinary)
+
+    def onClose(self, wasClean, code, reason):
+        self.factory.unregister(self)
+
+
+class BroadcastServerFactory(WebSocketServerFactory):
+
+    """ 
+    Simple broadcast server broadcasting any message it receives to all
+    currently connected clients.
+    """
+    protocol = BroadcastServerProtocol
+
+    def __init__(self, url):
+        WebSocketServerFactory.__init__(self, url)
+        self.clients = []
+
+    def register(self, client):
+        if client not in self.clients:
+            self.clients.append(client)
+            print("registered client {}".format(client.peer))
+
+    def unregister(self, client):
+        if client in self.clients:
+            self.clients.remove(client)
+            print("unregistered client {}".format(client.peer))
+
+    def broadcast(self, payload, isBinary):
+        for c in self.clients:
+            c.sendMessage(payload, isBinary)
+        print("broadcasted message to {} clients".format(len(self.clients)))
 
 def mode_callback(data):
     AUVMODE = data.auvmode
@@ -33,16 +76,40 @@ def listener():
     rospy.init_node('joystick_executor', anonymous=True)
     rospy.Subscriber('current_mode', mode, mode_callback)
 
-    rate = rospy.Rate(5)
-    # TODO
-    sendtraj = trajectory()
-    sendmode = mode()
-    sendmode.auvmode = True; sendmode.rovmode = False
-    modepub.publish(sendmode)
-    while not rospy.is_shutdown():
-        pub.publish(sendtraj)
-        rate.sleep()
+    factory = None
+    simmode = True # TODO: Allow command line switch of this 
+    if simmode is True:
+        factory = BroadcastServerFactory(u"ws://127.0.0.1:9000")
+    if simmode is False:
+        factory = BroadcastServerFactory(u"ws://enbarr.local:9000")
 
+    try:
+        rate = rospy.Rate(5)
+        # TODO
+        sendtraj = trajectory()
+        sendmode = mode()
+        sendmode.auvmode = True; sendmode.rovmode = False
+        modepub.publish(sendmode)
+        while not rospy.is_shutdown():
+            trajpub.publish(sendtraj)
+            rate.sleep()
+
+    except Exception as e:
+        # If anything messes up, make sure the thrusters aren't spinning anymore
+        print(e)
+        sendtraj = trajectory()
+        trajectory.orientation.roll = 0
+        trajectory.orientation.pitch = 0
+        trajectory.orientation.yaw = 0
+        trajectory.translation.x = 0
+        trajectory.translation.y = 0
+        trajectory.translation.z = 0
+        sendmode.auvmode = True
+        sendmode.rovmode = False
+        sendmode = mode()
+
+        trajpub.publish(sendtraj)
+        modepub.publish(sendmode)
 
 if __name__ == '__main__':
     listener()
