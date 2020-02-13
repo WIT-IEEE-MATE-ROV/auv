ed1bf322d3eb444684507816d2d08f67a444ba15
Michael Rivnak <mrivnak86@gmail.com>
2020-02-13 18:32:47 -0500

    Added thread joins

--- core/filtering/kalman.cpp
+++ core/filtering/kalman.cpp
@@ -24,15 +24,15 @@
 ros::Publisher ninedof_filtered_pub;
 
 typedef struct {
-	float roll;
-	float pitch;
-	float yaw;
+    float roll;
+    float pitch;
+    float yaw;
 } orientation;
  
 typedef struct {
-	float x;
-	float y;
-	float z;
+    float x;
+    float y;
+    float z;
 } translation;
 
 void ninedofCallback(const auv::ninedof::ConstPtr& inMsg);
@@ -42,62 +42,71 @@ void kalman_filter(orientation *input);
 void kalman_thread(void *ptr);
 
 int main(int argc, char **argv) {
-	// Initialize ROS publisher
-	ros::init(argc, argv, "ninedof_filter_kalman");
-	ros::NodeHandle n;
-	ros::Subscriber ninedof_current_sub = n.subscribe("ninedof_values", 1000, &ninedofCallback);
-	ninedof_filtered_pub = n.advertise<auv::ninedof>("ninedof_filtered", 3);
+    // Initialize ROS publisher
+    ros::init(argc, argv, "ninedof_filter_kalman");
+    ros::NodeHandle n;
+    ros::Subscriber ninedof_current_sub = n.subscribe("ninedof_values", 1000, &ninedofCallback);
+    
+    ninedof_filtered_pub = n.advertise<auv::ninedof>("ninedof_filtered", 3);
 
-	ros::spin();
+    ros::spin();
 
-	return 0;
+    return 0;
 }
 
 void ninedofCallback(const auv::ninedof::ConstPtr& inMsg) {
-	auv::ninedof outMsg;
+    auv::ninedof outMsg;
 
-	orientation gyro = {
-		inMsg->orientation.roll,
-		inMsg->orientation.pitch,
-		inMsg->orientation.yaw
-	};
-	translation accel = {
-		inMsg->translation.x,
-		inMsg->translation.y,
-		inMsg->translation.z
-	};
+    orientation gyro = {
+        inMsg->orientation.roll,
+        inMsg->orientation.pitch,
+        inMsg->orientation.yaw
+    };
+    translation accel = {
+        inMsg->translation.x,
+        inMsg->translation.y,
+        inMsg->translation.z
+    };
 
-	kalman_filter(&gyro);
-	kalman_filter(&accel);
+    kalman_filter(&gyro);
+    kalman_filter(&accel);
 
-	outMsg.orientation.roll 	= gyro.roll;
-	outMsg.orientation.pitch 	= gyro.pitch;
-	outMsg.orientation.yaw 		= gyro.yaw;
-	outMsg.translation.x 		= accel.x;
-	outMsg.translation.y 		= accel.y;
-	outMsg.translation.z 		= accel.z;
+    outMsg.orientation.roll 	= gyro.roll;
+    outMsg.orientation.pitch 	= gyro.pitch;
+    outMsg.orientation.yaw 		= gyro.yaw;
+    outMsg.translation.x 		= accel.x;
+    outMsg.translation.y 		= accel.y;
+    outMsg.translation.z 		= accel.z;
 
-	ninedof_filtered_pub.publish(outMsg);
+    ninedof_filtered_pub.publish(outMsg);
 }
 
 void kalman_filter(orientation *input) {
-	std::vector<std::thread> threads;
-	
-	float * fields[] = {&input->roll, &input->pitch, &input->yaw};
+    std::vector<std::thread> threads;
+    
+    float * fields[] = {&input->roll, &input->pitch, &input->yaw};
 
-	for (float * field : fields) {
-		threads.push_back(std::thread(&kalman_thread, field));
-	}
+    for (float * field : fields) {
+        threads.push_back(std::thread(&kalman_thread, field));
+    }
+
+    for (std::thread& thread : threads) {
+        thread.join();
+    }
 }
 
 void kalman_filter(translation *input) {
-	std::vector<std::thread> threads;
+    std::vector<std::thread> threads;
 
-	float * fields[] = {&input->x, &input->y, &input->z};
+    float * fields[] = {&input->x, &input->y, &input->z};
 
-	for (float * field : fields) {
-		threads.push_back(std::thread(&kalman_thread, field));
-	}
+    for (float * field : fields) {
+        threads.push_back(std::thread(&kalman_thread, field));
+    }
+
+    for (std::thread& thread : threads) {
+        thread.join();
+    }
 }
 
 void kalman_thread(void *ptr) {
