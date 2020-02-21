065f223f5ef7348278d30f572f8f9d36cf5f29e2
Michael Rivnak <mrivnak86@gmail.com>
2020-02-21 11:56:53 -0500

    Kalman filter works

--- core/filtering/kalman_bucy.py
+++ core/filtering/kalman_bucy.py
@@ -23,6 +23,7 @@ import math
 import numpy as np
 import random
 import scipy as sp
+from scipy import linalg
 
 import rospy
 import auv.msg as auv
@@ -36,13 +37,6 @@ Publisher = rospy.Publisher('ninedof_filtered', auv.ninedof, queue_size=3)
 history = []
 
 
-def callback_ninedof(data):
-    msg = auv.ninedof()
-
-    global Publisher
-    Publisher.publish(msg)
-
-
 class _Kalman:
     N = 2  # dimension of state
     I = np.eye(N)  # identity operator
@@ -53,39 +47,95 @@ class _Kalman:
     sd = 10  # choose random number seed
     A = np.array([[-1, 1], [-1, -1]])  # dynamics determined by A
     tau = 0.01  # time discretization is tau
-    L = sp.linalg.expm(A*tau)  # forward semigroup operator
+    L = linalg.expm(A*tau)  # forward semigroup operator
     Sigma = (sigma**2) * (np.linalg.lstsq(A + A.transpose(), (L * L.transpose() - I)))  # dynamics noise variance integrated
-    sqrtS = sp.linalg.sqrtm(Sigma)
+    Sigma = Sigma[0]
+
+    sqrtS = linalg.sqrtm(Sigma)
 
     def __init__(self):
         random.seed(self.sd)
 
-        self.m = np.array([[]])
-        self.v = np.array([[]])
-        self.z = np.array([])
-        self.c = np.array([[[]]])
-        self.v[:, 0] = self.m0 + sp.linalg.sqrtm(self.C0) * np.random.randn(self.N, 1)  # initial truth
-        self.m[:, 0] = 10 * np.random.randn(self.N, 1)  # initial mean/estimate
-        self.c[:, :, 0] = 100 * self.C0  # initial covariance
+        self.m = np.zeros((2,1), float)
+        self.v = np.zeros((2,2), float)
+        self.z = np.zeros((2,2), float)
+        self.c = np.zeros((2,2), float)
+        self.v = self.m0 + linalg.sqrtm(self.C0) * np.random.randn(self.N, 1)  # initial truth
+        self.m = 10 * np.random.randn(self.N, 1)  # initial mean/estimate
+        self.c = 100 * self.C0  # initial covariance
         self.z[0] = 0  # initial ghost observation
-        self.H = [1, 0]  # observation operator
+        self.H = np.array([1, 0])  # observation operator
         
-        self.v_1 = np.array([[]])
-        self.z_1 = np.array([])        
-        self.m_1 = np.array([[]])        
-        self.c_1 = np.array([[[]]])
+        self.v_1 = np.zeros((2,2), float)
+        self.z_1 = np.zeros((1), float)       
+        self.m_1 = np.zeros((2,1), float)      
+        self.c_1 = np.zeros((2,2), float)
 
-    def filter(self):
-        self.v_1[:, 0] = self.L * self.v[:, 1] + self.sqrtS * np.random.randn(self.N, 1)  # truth
-        self.z_1[0] = self.z[0] + self.tau * self.H * self.v_1[:, 0] + self.gamma * np.sqrt(self.tau) * np.random.randn  # observation
+    def filter(self, point):
+        self.v_1 = self.L * self.v + self.sqrtS * np.random.randn(self.N, 1)  # truth
+        self.z_1 = self.z[0] + self.tau * self.H * self.v_1 + self.gamma * np.sqrt(self.tau) * point  # observation
         
-        mhat = self.L * self.m[:, 1]  # estimator intermediate "predict"
-        chat = self.L * self.c[:, :, 1] * self.L.transpose() + self.Sigma  # covariance intermediate "predict"
+        mhat = self.L * self.m  # estimator intermediate "predict"
+        chat = self.L * self.c * self.L.transpose() + self.Sigma  # covariance intermediate "predict"
         
         d = (self.z_1[0] - self.z[0]) / self.tau - self.H * mhat  # "innovation"
         K = (self.tau * chat * self.H.transpose()) / (self.H * chat * self.H.transpose() * self.tau + self.gamma**2)  # "Kalman gain"
-        self.m_1[:, 0] = mhat + K * d  # estimator "update"
-        self.c_1[:, :, 0] = (self.I - K * self.H) * chat  # covariance "update"
+        self.m_1 = mhat + K * d  # estimator "update"
+        self.c_1 = (self.I - K * self.H) * chat  # covariance "update"
+        self.m = self.m_1
+        self.v = self.v_1
+        self.z = self.z_1
+        self.c = self.c_1
+
+        return self.m[0][0]
+
+
+class Kalman:
+    gyro_roll = _Kalman()
+    gyro_pitch = _Kalman()
+    gyro_yaw = _Kalman()
+    accel_x = _Kalman()
+    accel_y = _Kalman()
+    accel_z = _Kalman()
+
+    @staticmethod
+    def filter(point):
+        out_msg = auv.ninedof()
+
+        out_msg.orientation.roll = Kalman.gyro_roll.filter(point.orientation.roll)
+        out_msg.orientation.pitch = Kalman.gyro_pitch.filter(point.orientation.pitch)
+        out_msg.orientation.yaw = Kalman.gyro_yaw.filter(point.orientation.yaw)
+        out_msg.translation.x = Kalman.accel_x.filter(point.translation.x)
+        out_msg.translation.y = Kalman.accel_y.filter(point.translation.y)
+        out_msg.translation.z = Kalman.accel_z.filter(point.translation.z)
+
+        return out_msg
+
+
+def callback_ninedof(data):
+
+    rospy.logdebug('Input: {:.3f};{:.3f};{:.3f};{:.3f};{:.3f};{:.3f};'.format(
+        data.orientation.roll,
+        data.orientation.pitch,
+        data.orientation.yaw,
+        data.translation.x,
+        data.translation.y,
+        data.translation.z
+        ))
+
+    msg = Kalman.filter(data)
+
+    rospy.logdebug('Output: {:.3f};{:.3f};{:.3f};{:.3f};{:.3f};{:.3f};'.format(
+        msg.orientation.roll,
+        msg.orientation.pitch,
+        msg.orientation.yaw,
+        msg.translation.x,
+        msg.translation.y,
+        msg.translation.z
+        ))
+
+    global Publisher
+    Publisher.publish(msg)
 
 
 if __name__ == '__main__':
@@ -94,23 +144,3 @@ if __name__ == '__main__':
 
     rospy.logwarn("The kalman filter being used has not been thoroughly tested, and has not been tuned.")
     rospy.spin()
-
-
-class Orientation:
-    def __init__(self, roll, pitch, yaw):
-        self.roll = roll if roll else 0.0
-        self.pitch = pitch if pitch else 0.0
-        self.yaw = yaw if yaw else 0.0
-
-
-class Translation:
-    def __init__(self, x, y, z):
-        self.x = x if x else 0.0
-        self.y = y if y else 0.0
-        self.z = z if z else 0.0
-
-
-class Ninedof:
-    def __init__(self, orientation, translation):
-        self.translation = translation if translation else None
-        self.orientation = orientation if orientation else None
